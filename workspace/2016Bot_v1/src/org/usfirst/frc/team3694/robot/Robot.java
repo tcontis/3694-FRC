bc248ca7cba8ae5869f69c976df18976a5d59cdd
Thomas Contis <thomas@contis.org>
2016-02-02 20:40:01 -0500

    Polished and commented code.

--- workspace/2016Bot_v1/src/org/usfirst/frc/team3694/robot/Robot.java
+++ workspace/2016Bot_v1/src/org/usfirst/frc/team3694/robot/Robot.java
@@ -1,7 +1,7 @@
 //Defines stuff
 package org.usfirst.frc.team3694.robot;
 
-//import stuff here
+//Import stuff here
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
 
@@ -17,30 +17,40 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 //ROBOT CODE FROM THIS POINT ON
 public class Robot extends SampleRobot {
-    int session;
-    Image frame;
-	RobotDrive chassis;
-    Joystick driveStick;
-    Joystick shootStick;
-    Victor frontDrive = new Victor(0);
-    Victor rearDrive = new Victor(1);
-    Victor roller = new Victor(3);
-    Victor rollerTilt = new Victor(4);
-    SendableChooser chooser;
+	//NIVision Objects and Variables
+	public static int session;
+	public static Image frame;
+	
+	//SmartDashboard Objects and Variables
+	public static SendableChooser chooser;
+	public static String defense;
+	
+	//Drive and Chassis Objects and Variables
+	public static RobotDrive chassis;
+	public static Victor frontDrive = new Victor(0);
+	public static Victor rearDrive = new Victor(1);
+	public static Victor roller = new Victor(3);
+	public static Victor rollerTilt = new Victor(4);
+	
+	//Joystick Objects and Variables
+	public static Joystick driveStick = new Joystick(0);
+	public static Joystick shootStick = new Joystick(1);
+	public static double shootY;
+
+	
 
 //ROBOT INIZILIZATION
     public void robotInit() {
+    	//Configure Image and Session for NIVISIon
     	frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
         session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        
+        //Stream and Capture Image
         NIVision.IMAQdxConfigureGrab(session);
         CameraServer.getInstance().setImage(frame);
-    	SendableChooser chooser = new SendableChooser();
-    	frontDrive.setInverted(true);
-    	rearDrive.setInverted(true);
-    	chassis = new RobotDrive(frontDrive, rearDrive);
-    	chassis.setExpiration(0.1);
-    	driveStick = new Joystick(0);
-    	shootStick = new Joystick(1);
+    	
+        //Initialize SmartDashboard Autonomous Defense Chooser with Options
+        SendableChooser chooser = new SendableChooser();
     	chooser.initTable(NetworkTable.getTable("Defense Chooser"));
     	chooser.addDefault("Low Bar", "lowbar");
     	chooser.addObject("Portcullis" ,"portcullis"); 
@@ -52,45 +62,67 @@ public class Robot extends SampleRobot {
     	chooser.addObject("Rock Wall" ,"rockwall");
     	chooser.addObject("Rough Terrain" ,"roughterrain");
     	SmartDashboard.putData("Defense Chooser", chooser);
+    	
+    	//Invert Chassis Motors so that they roll in the right direction
+    	frontDrive.setInverted(true);
+    	rearDrive.setInverted(true);
+    	
+    	//Configure motors present in Chassis, configure safety
+    	chassis = new RobotDrive(frontDrive, rearDrive);
+    	chassis.setExpiration(0.1);
     }
 
 //AUTONOMOUS
     public void autonomous() {
-    		String defense = chooser.getSelected().toString();
-    		if(defense.equals("lowbar")){
-    			 chassis.setSafetyEnabled(false);
-    	         chassis.drive(0.5, 0.0);	// drive forwards half speed
-    	         Timer.delay(2.0);		//    for 2 seconds
-    	         chassis.drive(0.0, 0.0);	// stop robot	
-    		}else if(defense.equals("portcullis")){
-    			
-    		}else if(defense.equals("cheval")){
-    			
-    		}else if(defense.equals("moat")){
-    			
-    		}else if(defense.equals("ramparts")){
-    			
-    		}else if(defense.equals("drawbridge")){
-    			
-    		}else if(defense.equals("sallyport")){
-    			
-    		}else if(defense.equals("rockwall")){
-    			
-    		}else if(defense.equals("roughterrain")){
-    			
-    		}else{
-    			SmartDashboard.putString("Error", "No defenses match");
-    		}
-    		}
+    	//Set a string to find out the defense chosen
+    	defense = chooser.getSelected().toString();
+    	
+    	//Start of loops to figure out which defense selected
+    	if(defense.equals("lowbar")){
+    		//Lowbar autonomous
+    		chassis.setSafetyEnabled(false);
+    		
+    		//Drive forward for two seconds at half speed, the stop
+    	    chassis.drive(0.5, 0.0);	
+    	    Timer.delay(2.0);
+    	    chassis.drive(0.0, 0.0);		
+    	}else if(defense.equals("portcullis")){
+    		//Portcullis autonomous
+    	}else if(defense.equals("cheval")){
+    		//Cheval de Frise autonomous
+    	}else if(defense.equals("moat")){
+    		//Moat autonomous
+    	}else if(defense.equals("ramparts")){
+    		//Rampart autonomous
+    	}else if(defense.equals("drawbridge")){
+    		//Drawbridge autonomous
+    	}else if(defense.equals("sallyport")){
+    		//Sallyport autonomous
+    	}else if(defense.equals("rockwall")){
+    		//Rock Wall autonomous
+    	}else if(defense.equals("roughterrain")){
+    		//Rough Terrain autonomous
+    	}else{
+    		//If none of the above are the selected defense, then there is an error
+    		SmartDashboard.putString("Error", "No defenses match");
+    	}
+    }
     		
 //TELEOPERATED
     public void operatorControl() {
-    	 chassis.setSafetyEnabled(true);
-        //while under in Teleoperation mode.
+    	//Enable Chassis Safety
+    	chassis.setSafetyEnabled(true);
+        
+    	//While in Teleoperated mode.
         while (isOperatorControl() && isEnabled()) {
-            Timer.delay(0.005);
+            //Slight delay required
+        	Timer.delay(0.005);
+            
+        	//Drive chassis using Arcade Drive (One Joystick)
             chassis.arcadeDrive(driveStick);
-            double shootY = shootStick.getAxis(Joystick.AxisType.kY);
+            
+            //Set the roller's tilt to be equal to the Shooting Joystick's Y
+            shootY = shootStick.getAxis(Joystick.AxisType.kY);
             rollerTilt.set(shootY);
         }
     }
