89f90d9c460e661be569ed884bf3761146052978
MasterSamurai <thomas@contis.org>
2016-04-04 11:27:23 -0400

    See Header

--- workspace/El_Diablo/src/org/usfirst/frc/team3694/robot/Robot.java
+++ workspace/El_Diablo/src/org/usfirst/frc/team3694/robot/Robot.java
@@ -3,10 +3,13 @@
  * FRC 2016 Robot Code
  * "El Diablo"
  * 
- * Version 1.1.0
+ * Version 1.1.1
  * 
  * Changes: 
  * -Fixed autonomous
+ * -Added new Autonomous methods
+ * -Removed unused code
+ * -Added toggle on joystick
  * -Tweaks
 \***************************************************/
 
@@ -20,14 +23,12 @@ import edu.wpi.first.wpilibj.networktables.*;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 
 //ROBOT CODE FROM THIS POINT ON
-public class Robot extends IterativeRobot {
+public class Robot extends IterativeRobot{
 	//SmartDashboard Objects and Variables
 	public static SendableChooser chooser; //Destination Point
 	public static SendableChooser chooser3; //Camera
 	public static int status; //Camera Status
-	public static int autoChooser; //Emergency variable :P
-	public static int rawDist; //Int distance between points
-	public static double dist; //Encoder distance between points
+	public static int autoChooser; //Autonomous chooser
 	
 	//PWM Objects and Variables
 	public static RobotDrive chassis; //Robot Chassis
@@ -40,11 +41,13 @@ public class Robot extends IterativeRobot {
 	//USB Objects and Variables
 	public static Joystick driveStick = new Joystick(0); //Joystick used for Driving------------------------------------------USB (0)
 	public static Joystick shootStick = new Joystick(1); //Joystick used for Shooting-----------------------------------------USB (1)
+	public static boolean if4 = false;
+	public static boolean if5 = false;
 	
 	//SPI Objects and Variables
 	public static final double Kp = 0.03; //proportional scaling constant
-	public static ADXL362 accel = new ADXL362(SPI.Port.kOnboardCS1, Accelerometer.Range.k16G); //Accelerometer----------------SPI (1)
 	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0); //Gyroscope-----------------------------------SPI (0)
+	public static ADXL362 accel = new ADXL362(SPI.Port.kOnboardCS1, Accelerometer.Range.k16G); //Accelerometer----------------SPI (1)
 
 	//DIO Objects and Variables
 	public static Encoder leftEncoder = new Encoder(0, 1, true, Encoder.EncodingType.k4X); //Left Side Driver Encoder---------DIO (0,1)
@@ -53,91 +56,42 @@ public class Robot extends IterativeRobot {
 	public static DigitalInput rollerUpSwitch = new DigitalInput(7); //Roller Upper Limit Switch------------------------------DIO (7)
 	public static DigitalInput rollerDownSwitch = new DigitalInput(8); //Roller Lower Limit Switch----------------------------DIO (8)
 	public static DigitalInput rollerBallSwitch = new DigitalInput(9); //Roller Ball Limit Switch-----------------------------DIO (9)
-	public static final double distPerRev = 4.05 * Math.PI; //Distance per revolution
-	public static final double distPerCount = distPerRev/249; //Distance per encoder count
+	public static final double circumference = 4.05 * Math.PI; //Distance per revolution
 
-//ROBOT INIZILIZATION (RUNS ONCE)
-	public void robotInit() {
-		if(status == 0){
-    		CameraServer.getInstance().startAutomaticCapture("cam0");
-    	}	
-		//Table Creation for Destination Point
-		chooser = new SendableChooser();
-		chooser.initTable(NetworkTable.getTable("Autonomous"));
-      	chooser.addDefault("Nothing", 0);
-      	chooser.addObject("Onwards!" , 1); 
-      	SmartDashboard.putData("Destination Point", chooser);
-      	
-        //Table selection for Camera Status
-      	chooser3 = new SendableChooser();
-      	chooser3.initTable(NetworkTable.getTable("RoboRio Camera"));
-      	chooser3.addDefault("Enable", 0);
-      	chooser3.addObject("Disable", 1); 
-      	SmartDashboard.putData("RoboRio Camera", chooser3);
-      	
-      	//Initialize SmartDashboard Fields
-      	//SmartDashboard.putString("Current Point", chooser2.getSelected().toString());
-      	//SmartDashboard.putString("Destination Point", chooser.getSelected().toString());
-      	SmartDashboard.putString("Roborio Camera Status:", chooser3.getSelected().toString());
-      	SmartDashboard.putString("Error","");
-      	dashVarUpdate(0 ,0, 0, 0, 0, 0, 0);
-      	SmartDashboard.putString("Roller Direction", "---");
-      	SmartDashboard.putString("Test Sequence", "---");
-      	resetEncoders(); //reset encoders
-      	
-    	//Configure motors present in Chassis, configure safety
-    	chassis = new RobotDrive(leftDrive, rightDrive);
-    	gyro.calibrate();
-    	chassis.setExpiration(0.1);
-    }
 //RESET ENCODERS
 	public void resetEncoders(){
 		leftEncoder.reset(); //Reset left encoder
     	rightEncoder.reset(); //Reset right encoder
-    	rollerEncoder.reset();
+    	rollerEncoder.reset(); //Reset arm encoder
 	}
 //UPDATE VARIABLES IN SMARTDASHBOARD
-	public void dashVarUpdate(double a, double b, double c, double d, double e, double f, double g){
+	public void dashVarUpdate(double a, double b, double c){
 		SmartDashboard.putNumber("Gyro Angle", a);
       	SmartDashboard.putNumber("Gyro Angle Dial", b);
       	SmartDashboard.putNumber("Gyro Rate", c);
-      	SmartDashboard.putNumber("Acceleration X-Axis", d);
-      	SmartDashboard.putNumber("Acceleration Y-Axis", e);
-      	SmartDashboard.putNumber("Acceleration Z-Axis", f);
-      	SmartDashboard.putNumber("Roller Encoder", g);
 	}
-//ENCODER MOVE COUNTS
+//MOVE ROBOT
     public void move(double dist, double speed){
-    	//double cntsNeeded = dist*distPerCount; //Counts needed for distance
-    	//double rnddCountsNeeded = dist; //Rounded Counts Needed
-    	//resetEncoders(); //reset encoders
-    	if(leftEncoder.get() > dist || rightEncoder.get() > dist){
+    	double revsToTravel = dist/circumference;
+    	if(leftEncoder.get() > revsToTravel || rightEncoder.get() > revsToTravel){
     		chassis.drive(0.0, 0.0);
-    		//resetEncoders(); //reset encoders
+    		resetEncoders(); //reset encoders
     	}else{
-    		leftDrive.set(-0.5);
-    		rightDrive.set(-0.5);
-    		if(leftEncoder.get() > rightEncoder.get()){
-    			rightDrive.set(rightDrive.get() + 0.1);
-    		}
-    		if(rightEncoder.get() > leftEncoder.get()){
-    			leftDrive.set(leftDrive.get() + 0.1);
-    		}
-    		if(leftEncoder.get() < rightEncoder.get()){
-    			rightDrive.set(rightDrive.get() - 0.1);
-    		}
-    		if(rightEncoder.get() < leftEncoder.get()){
-    			leftDrive.set(leftDrive.get() - 0.1);
-    		}
-    		chassis.drive(speed, (-(gyro.getAngle())*Kp)); //Drive until encoder counts met
+    		chassis.drive(speed, (-gyro.getAngle()*Kp)); //Drive until encoder counts met
     	}
     }
-//CREATE ROLLER BUTTON
-    public void rollerButton(int a, String b, double c){
-    	if(shootStick.getRawButton(a)){
-        	SmartDashboard.putString("Roller Direction", b);
-        	roller.set(c);
-        }
+//TURN ROBOT
+    public void turn(double angle){
+    	if(gyro.getAngle() > 0){
+    		while(gyro.getAngle() < angle){
+    			chassis.arcadeDrive(0.0, angle);
+    		}
+    	}else if (gyro.getAngle() < 0){
+    		while(gyro.getAngle() > angle){
+    			chassis.arcadeDrive(0.0, angle);
+    		}
+    	}
+    	gyro.reset();
     }
 //MOVE MANIPULATOR
     public void moveArm(Joystick stick){
@@ -150,11 +104,30 @@ public class Robot extends IterativeRobot {
 		} else {
 			rollerTilt.set(shootStick.getAxis(Joystick.AxisType.kY));
 		}
-
 		if (shootStick.getAxis(Joystick.AxisType.kY) == 0) {
 			rollerTilt.setPosition(motorPos); // set rollerTilt to previous position	
 		}
     }
+//MOVE OVER DEFENSE AND SHOOT AUTO
+	public void fullAuto(double move1, double move2, double angle){
+	    SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+		while(rollerBallSwitch.get() == false){
+			roller.set(-0.75);
+		}
+		while(rollerDownSwitch.get() == false){
+			rollerTilt.set(0.5);
+		}
+		move(move1, -0.6);
+		while(rollerUpSwitch.get() == false){
+			rollerTilt.set(-0.5);
+		}
+		turn(angle);
+		move(move2, -0.6);
+		while(rollerDownSwitch.get() == false){
+			rollerTilt.set(0.5);
+		}
+		roller.set(0.75);
+	}
 //SEQUENCE CREATOR
     public void sequenceCreator(Victor v){
     	Timer.delay(0.5);
@@ -194,6 +167,45 @@ public class Robot extends IterativeRobot {
 		v.set(0.0);
 		Timer.delay(0.25);
     }
+//ROBOT INIZILIZATION (RUNS ONCE)
+	public void robotInit() {
+		//Start Camera
+		if(status == 0){
+    		CameraServer.getInstance().startAutomaticCapture("cam0");
+    	}	
+		//Table Creation for Destination Point
+		chooser = new SendableChooser();
+		chooser.initTable(NetworkTable.getTable("Autonomous"));
+      	chooser.addDefault("Nothing", 0);
+      	chooser.addObject("Forward" , 1); 
+      	chooser.addObject("Full Auto [0] [] [] [] []" , 2);
+      	chooser.addObject("Full Auto [] [0] [] [] []" , 3);
+      	chooser.addObject("Full Auto [] [] [0] [] []" , 4);
+      	chooser.addObject("Full Auto [] [] [] [0] []" , 5);
+    	chooser.addObject("Full Auto [] [] [] [] [0]" , 6);
+      	SmartDashboard.putData("Destination Point", chooser);
+      	
+      	//Table selection for Camera Status 
+      	chooser3 = new SendableChooser(); 
+      	chooser3.initTable(NetworkTable.getTable("RoboRio Camera")); 
+      	chooser3.addDefault("Enable", 0); 
+      	chooser3.addObject("Disable", 1);  
+      	SmartDashboard.putData("RoboRio Camera", chooser3); 
+      	
+      	//Initialize SmartDashboard Fields
+      	dashVarUpdate(0, 0, 0);
+        SmartDashboard.putString("Roborio Camera Status:", chooser3.getSelected().toString()); 
+      	SmartDashboard.putString("Roller Direction", "---");
+      	SmartDashboard.putString("Test Sequence", "---");
+      	SmartDashboard.putString("Error","---");
+      	SmartDashboard.putString("Console","---");
+      	
+    	//Configure motors present in Chassis, configure safety
+    	chassis = new RobotDrive(leftDrive, rightDrive);
+    	gyro.calibrate();
+    	chassis.setExpiration(0.1);
+      	resetEncoders(); //reset encoders
+    }
 //AUTONOMOUS INITIATION (RUNS ONCE)
     public void autonomousInit() {
     	Timer.delay(0.005); //Slight delay required
@@ -203,49 +215,94 @@ public class Robot extends IterativeRobot {
     	
     	//Get options we selected before autonomous
     	autoChooser = Integer.parseInt(chooser.getSelected().toString());
-    	//Camera Status
-  
-    	 if(isEnabled()){
-    	    	if(autoChooser == 0){
-    	    		SmartDashboard.putString("Error", "You chose Nothing as one of the points"); //Display error
-    	    	}else{
-    	    		move(10, -0.6);
-    	    	}
-    	    }
+    	if(isEnabled()){
+   		 	switch(autoChooser){ 
+   		 		case 0: SmartDashboard.putString("Console", "The robot will do nothing."); 
+   		 				break;  
+   		 		case 1: SmartDashboard.putString("Console", "The robot will move onwards."); 
+   		    			move(259, -0.6); 
+   		    			break;
+   		 		case 2: SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+   		 				fullAuto(290, 124, 30); // add robot length to 259 
+   		 				break;
+   		 		case 3: SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+   		 				fullAuto(312, 68, 30); // add robot length to 281
+   		 				break;
+	   		 	case 4: SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+	   		 			fullAuto(0, 0, 30);
+	   		 			break;
+		   		case 5: SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+		   		 		fullAuto(0, 0, -30);
+	   		 			break;
+		   		case 6: SmartDashboard.putString("Console", "The robot will move onwards and shoot at the low goal.");
+		   				fullAuto(0, 0, -30);
+	   		 			break;
+   		 	}
+    	}
     }
 //AUTONOMOUS PERIODIC (CALLED EVERY FIELD CYCLE)  
     public void autonomousPeriodic(){
-    	dashVarUpdate(gyro.getAngle(), gyro.getAngle(), gyro.getRate(), accel.getX(), accel.getY(), accel.getZ(), 0); //Update SmartDashboard Values
-    //Autonomous Move
-   
+    	dashVarUpdate(gyro.getAngle(), gyro.getAngle(), gyro.getRate()); //Update SmartDashboard Values
     }
 //TELEOPERATED INITIATION (RUNS ONCE)
     public void teleopInit() {
-    	chassis.setSafetyEnabled(false); //Enable Safety
+    	chassis.setSafetyEnabled(true); //Enable Safety
     	resetEncoders(); //reset encoders
     	gyro.reset();
     }
 //TELEOPERATED PERIODIC (CALLED EVERY FIELD CYCLE)
     public void teleopPeriodic(){
-    		Timer.delay(0.001); //Slight delay required
-        	dashVarUpdate(gyro.getAngle(), gyro.getAngle(), gyro.getRate(), accel.getX(), accel.getY(), accel.getZ(), rollerEncoder.get()); //Update SmartDashboard Values
-         	chassis.arcadeDrive(driveStick); //Drive chassis using Arcade Drive (One Joystick)
-            moveArm(shootStick); //Move manipulator arm
-            
-            //Roller Button Functions
-            rollerButton(3, "Stopped", 0.0); //Stop Roller-----------------------ShootStick (3)
-            if (rollerBallSwitch.get() == true) {
-    			rollerButton(4, "Cannot Go Backwards", 0.0);//Can't go Backwards
-    			roller.set(0.0);
-    		} else {
-    			rollerButton(4, "Backwards", -0.75);//Roll Roller Backwards----ShootStick (4)
-    		} 
-            rollerButton(5, "Forwards", 0.75); //Roll Roller Forwards----------ShootStick (5)
-     
-            if(driveStick.getRawButton(2)){ //Manually reset Gyro--------------DriveStick (2)
-            	gyro.reset();
-            }
-     }
+	    Timer.delay(0.005); //Slight delay required
+	    dashVarUpdate(gyro.getAngle(), gyro.getAngle(), gyro.getRate()); //Update SmartDashboard Values
+	    chassis.arcadeDrive(driveStick); //Drive chassis using Arcade Drive (One Joystick)
+	    moveArm(shootStick); //Move manipulator arm
+	    
+	    //Roller Button Functions 
+        if(shootStick.getRawButton(3)){ 
+	        if4 = false; 
+	        if5 = false; 
+	        SmartDashboard.putString("Roller Direction", "Stopped"); 
+	        roller.set(0.0); 
+        } //Stop Roller-----------------------ShootStick (3) 
+        if (rollerBallSwitch.get() == true) { 
+        	if(shootStick.getRawButton(4)){ 
+        		if5 = false; 
+        		SmartDashboard.putString("Roller Direction", "Cannot go Backwards!"); 
+        		roller.set(0.0); 
+            }//Can't go Backwards 
+        	roller.set(0.0); 
+		} else { 
+			if(shootStick.getRawButton(4)){ 
+				if5 = false; 
+				if(if4 == false){ 
+		            SmartDashboard.putString("Roller Direction", "Backwards"); 
+		            roller.set(-0.75); 
+		            if4 = true; 
+				}else{ 
+					SmartDashboard.putString("Roller Direction", "Stopped"); 
+					roller.set(0.0); 
+					if4 = false; 
+				} 
+		    }//Roll Roller Backwards----ShootStick (4) 
+		} 
+        if(shootStick.getRawButton(5)){ 
+        	if4 = false; 
+        	if(if5 == false){ 
+        		SmartDashboard.putString("Roller Direction", "Forwards"); 
+        		roller.set(0.75); 
+        		if5 = true; 
+        	}else{ 
+        		SmartDashboard.putString("Roller Direction", "Stopped"); 
+        		roller.set(0.0); 
+        		if5 = false; 
+        	} 
+        } //Roll Roller Forwards----------ShootStick (5) 
+         
+	    //Manual Gyro Reset
+	    if(driveStick.getRawButton(2)){ //Manually reset Gyro-------------------DriveStick (2)
+	    	gyro.reset();
+	    }
+}
 //TEST SEQUENCE (RUNS ONCE)
     public void testInit(){
     	if(isEnabled()){
@@ -260,7 +317,6 @@ public class Robot extends IterativeRobot {
     		SmartDashboard.putString("Test Sequence", "<!> Please keep your distance. The test will start in 10 seconds. <!>");
     		Timer.delay(10.0);
     		SmartDashboard.putString("Test Sequence", "Now testing Left Drive");
-    		
     		
     		//Left Drive Sequence (0 to 1.0, then back to 0. Then 0 to -1.0, then back to 0.
     		sequenceCreator(leftDrive);
