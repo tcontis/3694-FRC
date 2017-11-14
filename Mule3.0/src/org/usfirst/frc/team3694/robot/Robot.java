d1d8e55ef4728133b5b84875509a5e69b52c896e
zachlloyd01 <zachlloyd01@gmail.com>
2017-11-14 12:08:49 -0500

    Update Mule Code, Cleaned Repo

--- Mule3.0/src/org/usfirst/frc/team3694/robot/Robot.java
+++ Mule3.0/src/org/usfirst/frc/team3694/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,6 +23,9 @@ public class Robot extends IterativeRobot {
 		public static Joystick leftStick = new Joystick(0);
 		public static Joystick rightStick = new Joystick(1);
 		public static Joystick manStick = new Joystick(2);
+		
+	//Buttons
+		public static JoystickButton reverse = new JoystickButton(rightStick, 1);
 	
 	//timers
 		Timer autoTimer = new Timer();
@@ -37,28 +41,17 @@ public class Robot extends IterativeRobot {
 		Servo leftServo = new Servo(2);
 		Servo rightServo = new Servo(3);
 		
-	//Smart Dashboard stuffs
-		SendableChooser<String> chooser = new SendableChooser<>();
-		String autoSelected;
-			//choose stuff on the dashboard
-				final String DriveStraight = "Drive Straight";
-				final String Gear = "Gear";
+				
 		
 	@Override
 	public void robotInit() {
 		autonomous = new RobotDrive(left, right);
-		gyro = new AnalogGyro(1);
-		autonomous.setExpiration(0.1);
-		chooser.addDefault("Drive Straight", DriveStraight);
-		chooser.addObject("Place Gear", Gear);
-		SmartDashboard.putData("Auto Choices", chooser);
 		CameraServer.getInstance().startAutomaticCapture();
 	}
 	
 	@Override
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		System.out.println("Auto selected: " + autoSelected);
+
 	}
 	
 	@Override
@@ -77,42 +70,20 @@ public class Robot extends IterativeRobot {
 				right.set(-1 * rightStick.getY() * rightStick.getY());
 			}
 			while (manStick.getX() > 0) {
-				leftServo.set(-1 * manStick.getX());
-				rightServo.set(manStick.getX());
+				rightServo.set(-1 * manStick.getX());
+				leftServo.set(manStick.getX());
 			}
 			while (manStick.getX() < 0) {
 				leftServo.set(manStick.getX());
 				rightServo.set(-1 * leftStick.getX());
 			}
+			if (reverse.get() == true) {
+				left.set(leftStick.getY() * -1);
+				right.set(rightStick.getY() * -1);
+			}
 		}	
 	}
 	@Override
 	public void autonomousPeriodic() {
-		switch (autoSelected) {
-			case DriveStraight:
-				autoTimer.reset();
-				autoTimer.start();
-				gyro.reset();
-				while (isEnabled() && isAutonomous() && autoTimer.get() < 7) {
-					double angle = gyro.getAngle();
-					autonomous.drive(-1.0,  -angle * kp);
-					Timer.delay(0.004);
-				}
-				autonomous.drive(0, 0);
-				autoTimer.stop();
-			break;
-			case Gear:
-				autoTimer.reset();
-				autoTimer.start();
-				gyro.reset();
-				while (isEnabled() && isAutonomous() && autoTimer.get() < 3) {
-					double angle = gyro.getAngle();
-					autonomous.drive(0.4, -angle * kp);
-					Timer.delay(0.004);
-				}
-				autonomous.drive(0, 0);
-				autoTimer.stop();
-			break;
-		}
 	}
 }
--- SampleProgram/src/org/usfirst/frc/team3694/robot/Controls.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team3694.robot;
-
-import edu.wpi.first.wpilibj.Joystick;
-
-public class Controls {
-	public static Joystick leftStick = new Joystick(0);
-	public static Joystick rightStick = new Joystick(1);
-
-}
